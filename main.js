e208b7a430380974e244d07f309f6409f5fab6fb
Laasya ^_^~ <81613731+Laasya-tech@users.noreply.github.com>
2022-02-11 10:48:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function animals()
+{
+    window.location = "animals.html";
+}
+function Avengers()
+{
+    window.location = "Avengers.html";
+}
+function singers()
+{
+    window.location = "Singers.html";
+}
+function objects()
+{
+    window.location = "object.html";
+}
